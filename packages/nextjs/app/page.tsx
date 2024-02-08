c4c92b49038933e8f31ae35d7d293be069904f6d
👻 <y@uklok.ai>
2024-02-08 02:51:55 -0400

    feat(home): 👻 Checked In Counter
    - The counter was loaded from the contract using se-hooks.
    - `useState` and `useEffect` were used to update the interface correctly.
    
    This ref #5

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const [checkedInBuilders, setCheckedInBuilders] = useState(0n);
+  const { data: checkedInCounter } = useScaffoldContractRead({
+    contractName: "BatchRegistry",
+    functionName: "checkedInCounter",
+  });
+
+  useEffect(() => {
+    if (checkedInCounter !== undefined) {
+      setCheckedInBuilders(checkedInCounter);
+    }
+  }, [checkedInCounter]);
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
@@ -14,7 +30,7 @@ const Home: NextPage = () => {
           <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
           <p className="text-lg flex gap-2 justify-center">
             <span className="font-bold">Checked in builders count:</span>
-            <span>To Be Implemented</span>
+            <span>{checkedInBuilders.toString()}</span>
           </p>
         </div>
 
