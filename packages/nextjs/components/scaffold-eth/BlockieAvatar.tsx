f2da1f45fe37692155cade34816ff5e495530876
👻 <y@uklok.ai>
2024-02-08 03:38:55 -0400

    feat(components): 👻 isCrew & isCheckedIn.
    - If the address is part of the crew, the avatar becomes a Hexagon.
    - If isCrew then the online/offline indicator represents if it's checked in or not.
    
    This ref #4

--- packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
+++ packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { AvatarComponent } from "@rainbow-me/rainbowkit";
+import React from "react";
+import Image from "next/image";
+import { AvatarComponentProps } from "@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/AvatarContext";
 import { blo } from "blo";
 
+interface CustomAvatarProps extends AvatarComponentProps {
+  isCrew?: boolean;
+  isCheckedIn?: boolean;
+}
+type CustomAvatarComponent = React.FunctionComponent<CustomAvatarProps>;
+
 // Custom Avatar for RainbowKit
-export const BlockieAvatar: AvatarComponent = ({ address, ensImage, size }) => (
-  // Don't want to use nextJS Image here (and adding remote patterns for the URL)
-  // eslint-disable-next-line @next/next/no-img-element
-  <img
-    className="rounded-full"
-    src={ensImage || blo(address as `0x${string}`)}
-    width={size}
-    height={size}
-    alt={`${address} avatar`}
-  />
+export const BlockieAvatar: CustomAvatarComponent = ({ address, ensImage, size, isCrew, isCheckedIn }) => (
+  <div className={`avatar ${isCrew ? (isCheckedIn ? "online" : "offline") : ""}`}>
+    <div className={`w-8 ${isCrew ? "mask mask-hexagon-2" : "rounded-full"}`}>
+      <Image src={ensImage || blo(address as `0x${string}`)} width={size} height={size} alt={`${address} avatar`} />
+    </div>
+  </div>
 );
